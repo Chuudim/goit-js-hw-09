b32769b5b9f98cdb384f09bdd02f1001c6455ed1
Chuudim <chulanovdima2507@gmail.com>
2023-03-23 15:44:22 +0200

    ДЗ2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -21,4 +21,3 @@ const startButton = document.getElementById('startButton');
   function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
-с
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,45 +2,43 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
-  const dateTimePicker = document.getElementById("datetime-picker");
-  const startButton = document.querySelector("[data-start]");
-  const daysElement = document.querySelector("[data-days]");
-  const hoursElement = document.querySelector("[data-hours]");
-  const minutesElement = document.querySelector("[data-minutes]");
-  const secondsElement = document.querySelector("[data-seconds]");
+const dateTimePicker = document.getElementById("datetime-picker");
+const startButton = document.querySelector("[data-start]");
+const daysElement = document.querySelector("[data-days]");
+const hoursElement = document.querySelector("[data-hours]");
+const minutesElement = document.querySelector("[data-minutes]");
+const secondsElement = document.querySelector("[data-seconds]");
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate <= new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startButton.disabled = true;
+    } else {
+      Notiflix.Notify.success('Date is valid!');
+      startButton.disabled = false;
+    }
+  },
+};
 
-  const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
- onClose(selectedDates) {
-  const selectedDate = selectedDates[0];
-  if (selectedDate <= new Date()) {
-    Notiflix.Notify.failure('Please choose a date in the future');
-    startButton.disabled = true;
-  } else {
-    Notiflix.Notify.Success('Date is valid!');
-    startButton.disabled = false;
-  }
-},
-  };
+flatpickr(dateTimePicker, options);
 
-  // Ініціалізація flatpickr
-  flatpickr(dateTimePicker, options);
+let countdownIntervalId;
 
-  let countdownIntervalId;
-
-  startButton.addEventListener("click", () => {
-    startButton.disabled = true;
-    const selectedDate = new Date(dateTimePicker.value);
-    countdownIntervalId = setInterval(() => {
-      const timeLeft = selectedDate - new Date();
-      if (timeLeft < 0) {
-        clearInterval(countdownIntervalId);
-        return;
-      }
+startButton.addEventListener("click", () => {
+  startButton.disabled = true;
+  const selectedDate = new Date(dateTimePicker.value);
+  countdownIntervalId = setInterval(() => {
+    const timeLeft = selectedDate - new Date();
+    if (timeLeft < 0) {
+      clearInterval(countdownIntervalId);
+      return;
+    }
       const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
       const hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
       const minutes = Math.floor((timeLeft / (1000 * 60)) % 60);
